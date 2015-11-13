882d47e94f548e7a4ff3730ab1a2c3a1c76a2bb3
Eduardo Araujo <eduardo.araujo0@outlook.com>
2015-11-13 18:13:04 -0200

    Novas forms
    - Incluido o formUsuario 'linkando' as quatro principais ações do CRUD
    - Incluido o formLogin para 'linkar' as ações para o login de usuário

---
+++ GangOfSeven.OPEManager.Application/Forms/FormLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartNet.OPEManager.Data.Repositories;
+using SmartNet.OPEManager.Domain.Entities;
+
+namespace GangOfSeven.OPEManager.Application.Forms
+{
+    class FormLogin
+    {
+        public Boolean fazerLogin(int ra, String senha)
+        {
+            var usuarioRep = new UsuarioRep().BuscarPorId(ra);
+
+            if (usuarioRep.nome != null && usuarioRep.senha == senha)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }        
+    }
+}
---
+++ GangOfSeven.OPEManager.Application/Forms/FormUsuario.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartNet.OPEManager.Data.Repositories;
+using SmartNet.OPEManager.Domain.Entities;
+
+namespace GangOfSeven.OPEManager.Application.Forms
+{
+    class FormUsuario
+    {
+        public void incluirUsuario(int ra, String nome, String email, String senha, int tipoPerfil, Boolean status)
+        {
+            if (tipoPerfil == 1)
+            {
+                var professor = new Professor
+                {
+                    RA = ra,
+                    nome = nome,
+                    email = email,
+                    senha = senha,
+                    perfil = Usuario.Perfil.Administrador,
+                    ativo = status
+                };
+
+                var professorRep = new ProfessorRep().Adicionar(professor);
+            }
+            else
+            {
+                var aluno = new Aluno
+                {
+                    RA = ra,
+                    nome = nome,
+                    email = email,
+                    senha = senha,
+                    perfil = Usuario.Perfil.Usuario,
+                    ativo = status
+                };
+
+                var alunoRep = new AlunoRep().Adicionar(aluno);
+            }
+        }
+
+        public void alterarLogin(int ra, String nome, String email, String senha, int tipoPerfil, Boolean status)
+        {
+            if (tipoPerfil == 1)
+            {
+                var professor = new Professor
+                {
+                    RA = ra,
+                    nome = nome,
+                    email = email,
+                    senha = senha,
+                    perfil = Usuario.Perfil.Administrador,
+                    ativo = status
+                };
+
+                var professorRep = new ProfessorRep().Atualizar(professor);
+            }
+            else
+            {
+                var aluno = new Aluno
+                {
+                    RA = ra,
+                    nome = nome,
+                    email = email,
+                    senha = senha,
+                    perfil = Usuario.Perfil.Usuario,
+                    ativo = status
+                };
+
+                var alunoRep = new AlunoRep().Atualizar(aluno);
+            }
+        }
+
+        public void excluirLogin(int ra)
+        {
+            var usuario = new Usuario
+            {
+                RA = ra
+            };
+
+            new UsuarioRep().Remover(usuario);
+        }
+
+        public ICollection<Usuario> listarLogin()
+        {
+            ICollection<Usuario> usuarios = new UsuarioRep().BuscarTodos();
+
+            return usuarios;
+        }
+    }
+}
