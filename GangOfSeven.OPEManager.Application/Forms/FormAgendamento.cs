7d569092a28b46848f55022028604929de24df0b
Vitor Mantovani <vtrmantovani@gmail.com>
2015-11-23 19:38:06 -0200

    Adicionar Form de Agendamento

---
+++ GangOfSeven.OPEManager.Application/Forms/FormAgendamento.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartNet.OPEManager.Data.Repositories;
+using SmartNet.OPEManager.Domain.Entities;
+
+namespace GangOfSeven.OPEManager.Application.Forms
+{
+    class FormAgendamento
+    {
+        public void incluirAgendamento(Agendamento agendamento)
+        {
+            if (agendamento != null)
+            {
+                new AgendamentoRep().Adicionar(agendamento);
+            }
+        }
+
+        public void alterarAgendamento(Agendamento agendamento)
+        {
+            if (agendamento != null)
+            {
+                new AgendamentoRep().Atualizar(agendamento);
+            }
+        }
+
+        public void excluirAgendamento(Agendamento agendamento)
+        {
+            if (agendamento != null)
+            {
+                new AgendamentoRep().Remover(agendamento);
+            }
+        }
+
+        public ICollection<Agendamento> listarAgendamento()
+        {
+            ICollection<Agendamento> agendamentos = new AgendamentoRep().BuscarTodos();
+
+            return agendamentos;
+        }
+
+        public Agendamento BuscarPorId(int id)
+        {
+            return new AgendamentoRep().BuscarPorId(id);
+        }
+    }
+}
