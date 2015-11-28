687b5a61421a4256a6ffb8eb669eb3bdd0c22c0f
Vitor Mantovani <vtrmantovani@gmail.com>
2015-11-28 11:34:41 -0200

    Form de Fases

---
+++ GangOfSeven.OPEManager.Application/Forms/FormFase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartNet.OPEManager.Data.Repositories;
+using SmartNet.OPEManager.Domain.Entities;
+
+namespace GangOfSeven.OPEManager.Application.Forms
+{
+    class FormFase
+    {
+
+        public void incluirFase(Fase fase)
+        {
+            if (fase != null)
+            {
+                new FaseRep().Adicionar(fase);
+            }
+        }
+
+        public void alterarFase(Fase fase)
+        {
+            if (fase != null)
+            {
+                new FaseRep().Atualizar(fase);
+            }
+        }
+
+        public void excluirFase(int id)
+        {
+
+            var fase = new FaseRep().BuscarPorId(id);
+            fase.ativo = false;
+            new FaseRep().Atualizar(fase);
+
+        }
+
+        public ICollection<Fase> listarFases()
+        {
+            ICollection<Fase> fases = new FaseRep().BuscarTodos();
+
+            return fases;
+        }
+
+        public Fase BuscarPorId(int id)
+        {
+            return new FaseRep().BuscarPorId(id);
+        }
+    }
+}
