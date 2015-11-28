72fe95702bcb8a4cfd9701eaae326deb162a8ba8
Vitor Mantovani <vtrmantovani@gmail.com>
2015-11-28 11:46:59 -0200

    Form Comentario

---
+++ GangOfSeven.OPEManager.Application/Forms/FormComentario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartNet.OPEManager.Data.Repositories;
+using SmartNet.OPEManager.Domain.Entities;
+
+namespace GangOfSeven.OPEManager.Application.Forms
+{
+    class FormComentario
+    {
+        public void incluirComentario(Comentario comentario)
+        {
+            if (comentario != null)
+            {
+                new ComentarioRep().Adicionar(comentario);
+            }
+        }
+
+        public void alterarComentario(Comentario comentario)
+        {
+            if (comentario != null)
+            {
+                new ComentarioRep().Atualizar(comentario);
+            }
+        }
+
+        public void excluirComentario(int id)
+        {
+
+            var comentario = new ComentarioRep().BuscarPorId(id);
+            comentario.ativo = false;
+            new ComentarioRep().Atualizar(comentario);
+
+        }
+
+        public ICollection<Comentario> listarComentarios()
+        {
+            ICollection<Comentario> comentarios = new ComentarioRep().BuscarTodos();
+
+            return comentarios;
+        }
+
+        public Comentario BuscarPorId(int id)
+        {
+            return new ComentarioRep().BuscarPorId(id);
+        }
+    }
+}
