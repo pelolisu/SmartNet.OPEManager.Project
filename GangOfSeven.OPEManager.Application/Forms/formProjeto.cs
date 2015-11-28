78797efd657540e11c8b4b08e0301f2334641726
Vitor Mantovani <vtrmantovani@gmail.com>
2015-11-28 11:30:30 -0200

    Form Projeto

---
+++ GangOfSeven.OPEManager.Application/Forms/formProjeto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartNet.OPEManager.Data.Repositories;
+using SmartNet.OPEManager.Domain.Entities;
+
+namespace GangOfSeven.OPEManager.Application.Forms
+{
+    class formProjeto
+    {
+
+        public void incluirProjeto(Projeto projeto)
+        {
+            if (projeto != null)
+            {
+                new ProjetoRep().Adicionar(projeto);
+            }
+        }
+
+        public void alterarProjeto(Projeto projeto)
+        {
+            if (projeto != null)
+            {
+                new ProjetoRep().Atualizar(projeto);
+            }
+        }
+
+        public void excluirProjeto(int id)
+        {
+
+            var projeto = new ProjetoRep().BuscarPorId(id);
+            projeto.ativo = false;
+            new ProjetoRep().Atualizar(projeto);
+
+        }
+
+        public ICollection<Projeto> listarProjetos()
+        {
+            ICollection<Projeto> projetos = new ProjetoRep().BuscarTodos();
+
+            return projetos;
+        }
+
+        public Projeto BuscarPorId(int id)
+        {
+            return new ProjetoRep().BuscarPorId(id);
+        }
+    }
+}
