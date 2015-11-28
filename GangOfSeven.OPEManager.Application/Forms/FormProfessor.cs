9cf979a0b3e3f48852c55988e8c0ea61506f6184
Eduardo Araujo <eduardo.araujo0@outlook.com>
2015-11-28 15:42:17 -0200

    Incluido form de professor

---
+++ GangOfSeven.OPEManager.Application/Forms/FormProfessor.cs
@@ -0,0 +1,49 @@
+﻿using SmartNet.OPEManager.Data.Repositories;
+using SmartNet.OPEManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GangOfSeven.OPEManager.Application.Forms
+{
+    public class FormProfessor
+    {
+        public void incluirProfessor(Professor professor)
+        {
+            if (professor != null)
+            {
+                new ProfessorRep().Adicionar(professor);
+            }
+        }
+
+        public void alterarProfessor(Professor professor)
+        {
+            if (professor != null)
+            {
+                new ProfessorRep().Atualizar(professor);
+            }
+        }
+
+        public void excluirProfessor(int id)
+        {
+            var professor = new ProfessorRep().BuscarPorId(id);
+            professor.ativo = false;
+
+            new ProfessorRep().Atualizar(professor);
+        }
+
+        public ICollection<Professor> listarProfessor()
+        {
+            ICollection<Professor> professores = new ProfessorRep().BuscarTodos();
+
+            return professores;
+        }
+
+        public Professor BuscarPorId(int id)
+        {
+            return new ProfessorRep().BuscarPorId(id);
+        }
+    }
+}
